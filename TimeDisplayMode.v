6078f678e41bc50a9ec594b81372a2ee5fff0a38
veronica0512 <veronica3465@gmail.com>
2015-12-14 01:26:16 +0900

    Create TimeDisplayMode.v

---
+++ TimeDisplayMode.v
@@ -0,0 +1,21 @@
+module TimeDisplayMode(
+	input clk,
+	input sw,
+	input [5:0] hour_in,
+	input [5:0] min_in,
+	input [5:0] sec_in,
+	output am,
+	output pm,
+	output [5:0] hour_out,
+	output [5:0] min_out,
+	output [5:0] sec_out,
+	output ts
+    );
+
+wire enable;
+TimeDisplayModeSelector tdms(clk, sw, enable);
+TSmode tsmode(enable, ts, hour_in, min_in, sec_in, am, pm, hour_out, min_out, sec_out);
+
+
+endmodule
+Enter file contents here
